d72a35a61810455da2b38590324c3e77ac25ee62
Pauline Li <pli@teloslogistics.local>
2020-06-12 22:08:52 -0500

    Add files

--- UFO-level-1/static/js/index.js
+++ UFO-level-1/static/js/index.js
@@ -1,63 +1,53 @@
+var tdata = data;
+
+// console.log(tdata);
+
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // Console.log the weather data from data.js
-console.log(data);
+// console.log(tdata);
 
-// // Step 1: Loop Through `data` and console.log each weather report object
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-// });
 
-// // Step 2:  Use d3 to append one table row `tr` for each weather report object
-// // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
+// tdata.forEach((ufoReport) => {
 //   var row = tbody.append("tr");
-// });
-
-// // Step 3:  Use `Object.entries` to console.log each weather report value
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//   });
-// });
-
-// // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = row.append("td");
-//   });
-// });
-
-// // Step 5: Use d3 to update each cell's text with
-// // weather report values (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
+//   Object.entries(ufoReport).forEach(([key, value]) => {
 //     var cell = row.append("td");
 //     cell.text(value);
 //   });
 // });
 
-// BONUS: Refactor to use Arrow Functions!
-data.forEach((weatherReport) => {
-  var row = tbody.append("tr");
-  Object.entries(weatherReport).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
+
+var clickedButton = d3.select("#filter-btn");
+
+
+clickedButton.on("click", function() {
+  
+  // grab the value of the input field
+  //https://stackoverflow.com/questions/31369347/how-to-get-dynamic-value-of-input-element-with-d3
+  var textboxValue = d3.select("#datetime").property("value");
+   
+  var inputDate = textboxValue; 
+
+  console.log(inputDate)
+
+  var outputData =  tdata.filter(x => x.datetime === inputDate )
+
+  console.log(outputData);
+
+
+  outputData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
   });
+
 });
+
+   
+
+
+
+
